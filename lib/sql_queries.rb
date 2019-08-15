354348a1489e914471cda56a4144618389addd4d
Veronica Lopez <nicaa0695@gmail.com>
2019-08-15 17:06:36 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, SUM(pledges.amount)-project.funding_goal
+  "SELECT projects.title, SUM(pledges.amount)-projects.funding_goal
   AS diff FROM pledges JOIN projects ON projects.id = pledges.project_id GROUP BY pledges.project_id HAVING diff >= 0 ORDER BY projects.title"
 end
 
